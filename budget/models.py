d50e70af84e2705e68f45f3321a2f24c9e317fb7
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-07-11 02:32:54 +0200

    Add transfer methods & Update types&relations & Fix iterating wrong variable for received transactions

--- budget/models.py
+++ budget/models.py
@@ -1,5 +1,5 @@
+from typing import List, Optional
 from collections import Counter
-from datetime import datetime
 from decimal import Decimal
 
 from sqlmodel import Relationship, Field
@@ -8,9 +8,13 @@ from ..utils.mixins import Name, Timestamp, ID
 
 
 class Wallet(Name, table=True):
-    outgoing: list["Transaction"] = Relationship(back_populates="sender")
+    outgoing: List["Transaction"] = Relationship(
+        back_populates="sender", sa_relationship_kwargs={"foreign_keys": "Transaction.sender_id"}
+    )
     """List of all outgoing transactions"""
-    incoming: list["Transaction"] = Relationship(back_populates="recipent")
+    incoming: List["Transaction"] = Relationship(
+        back_populates="recipent", sa_relationship_kwargs={"foreign_keys": "Transaction.recipent_id"}
+    )
     """List of all incoming transactions"""
 
     @property
@@ -25,7 +29,7 @@ class Wallet(Name, table=True):
     def total_received(self) -> Counter:
         """Total incoming amounts by currency"""
         c = Counter()
-        for t in self.outgoing:
+        for t in self.incoming:
             c.update({t.currency: t.amount})
         return c
 
@@ -34,21 +38,63 @@ class Wallet(Name, table=True):
         """Remaining Balance (Received minus Sent)"""
         return self.total_received - self.total_sent
 
+    def add(self, value: float, currency: str, description: str) -> Decimal:
+        """Add funds to Wallet"""
+        self.incoming.append(Transaction(amount=value, currency=currency, description=description))
+        return self.balance.get(currency, 0)
+
+    def transfer(
+        self, amount: float, currency: str, description: str, recipent_id: int = None, recipent: "Wallet" = None
+    ) -> Decimal:
+        """
+        Transfer funds to another Wallet if there are enough funds in current Wallet
+
+        Parameters
+        ----------
+        amount:
+            Amount to transfer out of this wallet
+        currency:
+            Currency to transfer. Requires this currency being present on this wallet
+        description:
+            Description of this transaction
+        recipent_id:
+            Target's Wallet ID. Leave empty when using `recipent`
+        recipent:
+            Target's Wallet. Leave empty when using `recipent_id`
+        """
+        if self.balance.get(currency, 0) >= amount:
+            self.outgoing.append(
+                Transaction(
+                    amount=amount,
+                    currency=currency,
+                    description=description,
+                    recipent_id=recipent_id,
+                    recipent=recipent,
+                )
+            )
+        else:
+            print("Not enough funds to transfer")
+        return self.balance.get(currency, 0)
+
 
 class Transaction(Timestamp, ID, table=True):
     amount: Decimal
     """Amount transfered"""
-    currency: str
+    currency: str = Field(max_length=3)
     """Currency transfered"""
     description: str
     """Operation's description"""
 
-    sender_id: int = Field(foreign_key="wallet.id")
+    sender_id: Optional[int] = Field(foreign_key="wallet.id")
     """Wallet's source. Outgoing"""
-    recipent_id: int = Field(foreign_key="wallet.id")
+    recipent_id: Optional[int] = Field(foreign_key="wallet.id")
     """Wallet's destination. Incoming"""
 
-    sender: Wallet
+    sender: Optional[Wallet] = Relationship(
+        back_populates="outgoing", sa_relationship_kwargs={"foreign_keys": "Transaction.sender_id"}
+    )
     """Sender's wallet"""
-    recipent: Wallet
+    recipent: Optional[Wallet] = Relationship(
+        back_populates="incoming", sa_relationship_kwargs={"foreign_keys": "Transaction.recipent_id"}
+    )
     """Recipent's Wallet"""
