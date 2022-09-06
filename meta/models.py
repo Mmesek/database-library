bab9af3082b5e0120125118cf7fff94b537f3938
Mmesek <13630781+Mmesek@users.noreply.github.com>
2022-09-06 21:13:41 +0200

    Add User-Wallet link model & balance method

--- meta/models.py
+++ meta/models.py
@@ -1,7 +1,9 @@
-from sqlmodel import SQLModel
-
+from typing import List
+from sqlmodel import SQLModel, Field, Relationship
+from collections import Counter
 from datetime import datetime, timedelta
 from ..utils.mixins import Name, URL, ID, Timestamp
+from ..budget.models import Wallet
 
 
 class Quote(ID, table=True):
@@ -15,6 +17,7 @@ class Quote(ID, table=True):
 
 class Event(Timestamp, ID, table=True):
     """Wildcard event table"""
+
     description: str
     """Description of what this is about"""
     duration: timedelta
@@ -29,6 +32,11 @@ class Link(URL, Name, SQLModel, table=True):
     pass
 
 
+class User_Wallets(SQLModel, table=True):
+    user_id: int = Field(foreign_key="user.id", primary_key=True)
+    wallet_id: int = Field(foreign_key="wallet.id", primary_key=True)
+
+
 class User(Name):
     surname: str
     birthday: datetime
@@ -36,3 +44,12 @@ class User(Name):
     city: str
     street: str
     postal: str
+    wallets: List[Wallet] = Relationship(link_model=User_Wallets)
+
+    @property
+    def balance(self) -> Counter:
+        """Total balance across all wallets by currency"""
+        c = Counter()
+        for t in self.wallets:
+            c.update({t.currency: t.balance})
+        return c
