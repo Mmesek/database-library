1060e4f14b0da55e6ad328034b45263083efe92d
Mmesek <13630781+Mmesek@users.noreply.github.com>
2023-03-03 23:11:41 +0100

    Update bundle models to SQLAlchemy 2 style
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- bundles/models.py
+++ bundles/models.py
@@ -1,13 +1,16 @@
 import calendar
 
 from datetime import datetime
+from decimal import Decimal
 
-from sqlmodel import Field, Relationship
+from sqlalchemy.orm import Mapped, relationship as Relationship
 
-from ..utils.mixins import Name, ID, Timestamp, Price as MixinPrice
-from ..games.models import Game
+from mlib.database import ID, Default as Name, Timestamp, Base, Field
 
-TODAY = datetime.datetime.now()
+# from ..utils.mixins import Price as MixinPrice
+# from ..games.models import Game
+
+TODAY = datetime.now()
 NOW = TODAY.date()
 
 MONTHS = {
@@ -28,10 +31,15 @@ MONTHS = {
 c = calendar.Calendar(firstweekday=calendar.SUNDAY)
 
 
-class Game(Game):
+class MixinPrice:
+    price: Mapped[Decimal]
+    currency: Mapped[str]
+
+
+class Game(Name, Base):
     """Game metadata"""
 
-    keys: list["Key"]
+    keys: list["Key"] = Relationship("Key", back_populates="game")
 
     def find_bundle(self) -> "Bundle":
         i = next(filter(lambda x: not x.used, self.keys), None)
@@ -42,13 +50,13 @@ class Game(Game):
         return [key.bundle for key in self.keys if not key.used]
 
 
-class Bundle(MixinPrice, Name, table=True):
+class Bundle(MixinPrice, Name, Base):
     """Bundle metadata"""
 
-    keys: list["Key"] = Relationship(back_populates="bundle")
-    """Keys in this Bundle"""
-    date: datetime
+    date: Mapped[datetime]
     """Date bundle was purchased"""
+    keys: list["Key"] = Relationship("Key", back_populates="bundle")
+    """Keys in this Bundle"""
 
     def use_key(self, game: str) -> str:
         """Use key, returns key"""
@@ -67,36 +75,38 @@ class Bundle(MixinPrice, Name, table=True):
         return [key.game for key in self.keys]
 
 
-class Key(ID, table=True):
-    bundle_id: int = Field(foreign_key="bundle.id")
+class Key(ID, Base):
+    bundle_id: Mapped[int] = Field(foreign_key="Bundle.id")
     """Bundle ID which this key is associated with"""
-    bundle: Bundle = Relationship(back_populates="bundle.keys")
-    game_id: int = Field(foreign_key="game.id")
+    bundle: Bundle = Relationship(back_populates="Bundle.keys")
+    game_id: Mapped[int] = Field(foreign_key="Game.id")
     """Game this key unlocks"""
     game: Game = Relationship(back_populates="keys")
-    platform: str = Field(default="Steam", nullable=False)
-    """Platform this key is for"""
-    key: str = Field(nullable=True)
+    key: Mapped[str] = Field(nullable=True)
     """Key"""
-    used_date: datetime
+    used_date: Mapped[datetime] = Field(nullable=True)
     """Date when this key was used, if at all"""
+    expire_date: Mapped[datetime] = Field(nullable=True)
+    """Date when this key expires, if at all"""
+    platform: Mapped[str] = Field(default="Steam", nullable=False)
+    """Platform this key is for"""
 
     def use(self) -> str:
         self.used_date = datetime.now()
         return self.key
 
 
-class Offer(MixinPrice, Timestamp, ID, table=True):
+class Offer(MixinPrice, Timestamp, ID, Base):
     """Offer for this key"""
 
-    key_id: int
-    active: bool
+    key_id: Mapped[int]
+    active: Mapped[bool]
 
 
-class Price(MixinPrice, Timestamp, table=True):
+class Price(MixinPrice, Timestamp, Base):
     """Last fetched price for the game"""
 
-    game_id: int = Field(primary_key=True, foreign_key="game.id")
+    game_id: Mapped[int] = Field(primary_key=True, foreign_key="Game.id")
 
 
 def add_bundle(session, name, prc, currency, games):
