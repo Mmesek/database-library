f63164648845cdd55c5d7b2f9914abe37404bd72
Mmesek <13630781+Mmesek@users.noreply.github.com>
2024-11-19 23:20:51 +0100

    Add additional tables & localized tables
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,38 @@ from sqlmodel import SQLModel, Field, Relationship
 from ..utils.mixins import Name, ID
 
 
+class Ingredient_Alias(Name, table=True):
+    ingredient_id: int = Field(foreign_key="ingredient.id", primary_key=True)
+
+
+class Unit_Alias(Name, table=True):
+    unit_id: int = Field(foreign_key="unit.id", primary_key=True)
+
+
+class Unit(ID, table=True):
+    aliases: list[Unit_Alias]
+
+
+class Category(Name, table=True):
+    color: str
+
+
+class LocalizedIngredient(SQLModel, table=True):
+    locale: str = Field(primary_key=True)
+    ingredient_id: int = Field(foreign_key="ingredient.id", primary_key=True)
+    name: str
+    plural: str
+    abbreviation: str
+
+
+class LocalizedUnit(SQLModel, table=True):
+    locale: str = Field(primary_key=True)
+    unit_id: int = Field(foreign_key="unit.id", primary_key=True)
+    name: str
+    plural: str
+    abbreviation: str
+
+
 class Step_Ingredient(SQLModel, table=True):
     """Ingredient used in a recipe's step"""
 
@@ -14,15 +46,21 @@ class Step_Ingredient(SQLModel, table=True):
 
     quantity: float
     """Quantity of ingredient required for this step"""
-    unit: str
+    unit_id: int = Field(foreign_key="unit.id")
     """Unit in which quantity is provided"""
+    note: str
 
 
 class Ingredient(Name, table=True):
     """Ingredient metadata"""
 
-    steps: list["Step"] = Relationship(back_populates="ingredients", link_model=Step_Ingredient)
+    steps: list["Step"] = Relationship(
+        back_populates="ingredients", link_model=Step_Ingredient
+    )
     """Steps using this Ingredient"""
+    category_id: int
+    description: str
+    aliases: list[Ingredient_Alias]
 
 
 class Recipe(Name, table=True):
@@ -31,6 +69,7 @@ class Recipe(Name, table=True):
     description: str
     """Recipe Description"""
 
+    sub_recipes: list["Recipe"]  # List of sub recipes this recipe uses
     steps: list["Step"] = Relationship(back_populates="recipe")
     """Steps related to this Recipe"""
 
@@ -60,8 +99,16 @@ class Step(SQLModel, table=True):
 
     recipe: Recipe = Relationship(back_populates="steps")
     """Related Recipe"""
-    ingredients: list[Ingredient] = Relationship(back_populates="steps", link_model=Step_Ingredient)
+    ingredients: list[Ingredient] = Relationship(
+        back_populates="steps", link_model=Step_Ingredient
+    )
     """Ingredients used in this step"""
+
+
+class Step_Recipe:  # This would allow us to reuse steps between different recipes
+    recipe_id: int
+    step_id: int
+    ingredients: list[Step_Ingredient]
 
 
 class Ingredient_Inventory(ID, table=True):
@@ -71,7 +118,7 @@ class Ingredient_Inventory(ID, table=True):
     """ID of Ingredient"""
     quantity: float
     """Quantity currently owned"""
-    unit: str
+    unit: int = Field(foreign_key="unit.id")
     """Unit of quantity"""
     expire_date: datetime
     """Expiration date of an item"""
