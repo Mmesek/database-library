7d74ac63196255a9a149dd1e7e60b01b64628ba6
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-01-08 18:10:36 +0100

    Add support to fetch bid/ask rates
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- portfolio/fetch_nbp.py
+++ portfolio/fetch_nbp.py
@@ -8,12 +8,15 @@ def is_business(date: datetime):
     return bool(len(bdate_range(date, date)))
 
 
-def fetch(currency: str, year: int, month: int, day: int, previous_day: bool = True):
+def fetch(currency: str, year: int, month: int, day: int, previous_day: bool = True, avg: bool = True):
     date = datetime(year, month, day)
     if previous_day or not is_business(date):
         date = date - BusinessDay(1)
     res = httpx.request(
         "GET",
-        f"https://api.nbp.pl/api/exchangerates/rates/a/{currency}/{date.year}-{date.month:0>2}-{date.day:0>2}/?format=json",
+        f"https://api.nbp.pl/api/exchangerates/rates/{'a' if avg else 'c'}/{currency}/{date.year}-{date.month:0>2}-{date.day:0>2}/?format=json",
     ).json()
-    return res["rates"][0]["mid"]
+    if avg:
+        return res["rates"][0]["mid"]
+    else:
+        return res["rates"][0]["bid"], res["rates"][0]["ask"]
