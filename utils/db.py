5f3292499d4f10e30db5c07d3237b5f708668880
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-04-01 18:28:28 +0200

    Add switch to recreate tables
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- utils/db.py
+++ utils/db.py
@@ -2,6 +2,11 @@ from mlib.database import SQL, AsyncSQL
 from utils.env import HOST
 
 
-def make_session(database: str, driver: str = "postgresql+psycopg", class_: type[SQL | AsyncSQL] = SQL):
+def make_session(
+    database: str, driver: str = "postgresql+psycopg", class_: type[SQL | AsyncSQL] = SQL, start_fresh: bool = False
+):
     engine = class_(driver, location=HOST, name=database, echo=False)
+    if start_fresh:
+        engine.drop_tables()
+        engine.create_tables()
     return engine.session()
