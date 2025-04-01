179f3fe2aad536e16e1be66a77a2ba3cdb0e4413
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-04-01 18:36:57 +0200

    Update bundles SQLs
    Signed-off-by: Mmesek <13630781+Mmesek@users.noreply.github.com>

--- bundles/sql/bundled_total.sql
+++ bundles/sql/bundled_total.sql
@@ -12,3 +12,19 @@ CREATE VIEW "BundledTotal" AS
     LEFT JOIN "Price" ON "Key".game_id = "Price".game_id)
   WHERE ("Key".used_date IS NULL)
   GROUP BY "Bundle".id;
+
+  SELECT 
+    extract(year FROM "Bundle".date) AS year, 
+    "Bundle".name,
+    sum(COALESCE("Transaction".price, 0.0)) - "Bundle".price AS profit,
+    count(CASE WHEN "Key".used_date IS NULL THEN 1 ELSE NULL END) AS remaining
+  FROM "Key" 
+    LEFT JOIN "Transaction" ON "Transaction".key_id = "Key".id 
+    JOIN "Bundle" ON "Bundle".id = "Key".bundle_id 
+      AND "Bundle".id != 0 
+      AND COALESCE("Bundle".currency, 'PLN') = COALESCE("Transaction".currency, 'PLN')
+  GROUP BY "Bundle".id
+  ORDER BY 
+    remaining DESC, 
+    year DESC,
+    profit DESC
--- bundles/sql/bundledgames.sql
+++ bundles/sql/bundledgames.sql
@@ -1,31 +1,31 @@
 CREATE OR REPLACE VIEW "BundledGames" AS
-SELECT key.id,
-	bundle.date,
-	bundle.name AS bundle,
-	game.name AS game,
-	COALESCE(offer.price, 0.0) AS offer,
-	COALESCE(price.price, 0.0) AS price,
-	COALESCE((offer.price - price.price), 0.0) AS difference
+SELECT "Key".id,
+	"Bundle".date,
+	"Bundle".name AS bundle,
+	"Game".name AS game,
+	COALESCE("Offer".price, 0.0) AS offer,
+	COALESCE("Price".price, 0.0) AS price,
+	COALESCE(("Offer".price - "Price".price), 0.0) AS difference
 FROM (
 		(
 			(
 				(
-					key
-					JOIN bundle ON ((key.bundle_id = bundle.id))
+					"Key"
+					JOIN "Bundle" ON (("Key".bundle_id = "bundle".id))
 				)
-				JOIN game ON ((game.id = key.game_id))
+				JOIN "Game" ON (("Game".id = "Key".game_id))
 			)
-			LEFT JOIN offer ON ((key.id = offer.key_id))
+			LEFT JOIN "Offer" ON (("Key".id = "Offer".key_id))
 		)
-		LEFT JOIN price ON ((key.game_id = price.game_id))
+		LEFT JOIN "Price" ON (("Key".game_id = "Price".game_id))
 	)
-WHERE (key.used = false);
+WHERE ("Key".used_date IS NULL);
 
-CREATE OR REPLACE FUNCTION "use_key"() RETURNS trigger LANGUAGE plpgsql AS $ $ BEGIN
-UPDATE key
-SET used = True,
+CREATE OR REPLACE FUNCTION "use_key"() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN
+UPDATE "Key"
+SET
 	used_date = now()
-WHERE key.id = OLD.id;
+WHERE "Key".id = OLD.id;
 RETURN OLD;
 END;
 $$;
@@ -33,33 +33,77 @@ $$;
 CREATE OR REPLACE TRIGGER tg_use_key INSTEAD OF DELETE ON "BundledGames" FOR EACH ROW EXECUTE FUNCTION use_key ();
 CREATE EXTENSION pg_trgm;
 CREATE INDEX ON game USING gin(name gin_trgm_ops);
-SELECT key.id,
-	bundle.date,
-	bundle.name AS
+SELECT "Key".id,
+	"Bundle".date,
+	"Bundle".name AS
 	OR REPLACE TRIGGER tg_use_key INSTEAD OF DELETE ON "BundledGames" FOR EACH ROW EXECUTE FUNCTION use_key ();
 
 CREATE EXTENSION pg_trgm;
 CREATE INDEX ON game USING gin(name gin_trgm_ops);
-SELECT key.id,
-	bundle.date,
-	bundle.name AS bundle,
-	game.name AS game,
-	offer.price AS offer,
-	COALESCE(price.price, 0.0) AS price
+SELECT "Key".id,
+	"Bundle".date,
+	"Bundle".name AS bundle,
+	"Game".name AS game,
+	"Offer".price AS offer,
+	COALESCE("Price".price, 0.0) AS price
 FROM(
 		(
 			(
 				(
-					key
-					JOIN bundle ON((key.bundle_id = bundle.id))
+					"Key"
+					JOIN "Bundle" ON(("Key".bundle_id = "Bundle".id))
 				)
-				JOIN game ON((game.id = key.game_id))
+				JOIN game ON(("Game".id = "Key".game_id))
 			)
-			LEFT JOIN offer ON((key.id = offer.key_id))
+			LEFT JOIN offer ON(("Key".id = "Offer".key_id))
 		)
-		RIGHT JOIN price ON((key.game_id = price.game_id))
+		RIGHT JOIN price ON(("Key".game_id = "Price".game_id))
 	)
 WHERE(
-		(key.used = false)
-		AND(offer.price IS NULL)
+		("Key".used_date IS NULL)
+		AND("Offer".price IS NULL)
 	);
+
+
+
+CREATE VIEW "BundledGames" AS SELECT "Key".id,
+    "Bundle".date,
+    "Bundle".name AS bundle,
+    "Game".name AS game,
+    COALESCE("Offer".price, 0.0) AS offer,
+    COALESCE("Price".price, 0.0) AS price,
+    COALESCE(("Offer".price - "Price".price), 0.0) AS difference,
+    "Key".expire_date
+   FROM (((("Key"
+     JOIN "Bundle" ON (("Key".bundle_id = "Bundle".id)))
+     JOIN "Game" ON (("Game".id = "Key".game_id)))
+     LEFT JOIN "Offer" ON (("Key".id = "Offer".key_id)))
+     LEFT JOIN "Price" ON (("Key".game_id = "Price".game_id)))
+  WHERE ("Key".used_date IS NULL);
+
+DROP TABLE IF EXISTS "BundledTotal";
+CREATE VIEW "BundledTotal" AS SELECT "Bundle".date,
+    "Bundle".name AS bundle,
+    sum(COALESCE("Offer".price, 0.0)) AS offer,
+    sum(COALESCE("Price".price, 0.0)) AS price
+   FROM (((("Key"
+     JOIN "Bundle" ON (("Key".bundle_id = "Bundle".id)))
+     JOIN "Game" ON (("Game".id = "Key".game_id)))
+     LEFT JOIN "Offer" ON (("Key".id = "Offer".key_id)))
+     LEFT JOIN "Price" ON (("Key".game_id = "Price".game_id)))
+  WHERE ("Key".used_date IS NULL)
+  GROUP BY "Bundle".id;
+
+DROP TABLE IF EXISTS "NotListed";
+CREATE VIEW "NotListed" AS SELECT "Key".id,
+    "Bundle".date,
+    "Bundle".name AS bundle,
+    "Game".name AS game,
+    "Offer".price AS offer,
+    COALESCE("Price".price, 0.0) AS price
+   FROM (((("Key"
+     JOIN "Bundle" ON (("Key".bundle_id = "Bundle".id)))
+     JOIN "Game" ON (("Game".id = "Key".game_id)))
+     LEFT JOIN "Offer" ON (("Key".id = "Offer".key_id)))
+     RIGHT JOIN "Price" ON (("Key".game_id = "Price".game_id)))
+  WHERE (("Key".used_date IS NULL) AND ("Offer".price IS NULL));
