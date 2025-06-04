9313eaf499087d553641dfd2f6cdb2c91c408e8f
Mmesek <13630781+Mmesek@users.noreply.github.com>
2025-06-04 06:29:21 +0200

    Fix calculating min/max when transaction doesn't count

--- portfolio/sql/summary.sql
+++ portfolio/sql/summary.sql
@@ -1,10 +1,10 @@
 SELECT 
   asset, 
   sum(quantity), 
-  AVG(CASE WHEN quantity > 0 THEN price ELSE NULL END) AS avg_buy_price,
-  AVG(CASE WHEN quantity < 0 THEN price ELSE NULL END) AS avg_sell_price, 
-  min(price), 
-  max(price), 
+  AVG(CASE WHEN quantity > 0 AND price != 0 THEN price ELSE NULL END) AS avg_buy_price,
+  AVG(CASE WHEN quantity < 0 AND price != 0 THEN price ELSE NULL END) AS avg_sell_price, 
+  min(CASE WHEN price != 0 THEN price ELSE NULL END), 
+  max(CASE WHEN price != 0 THEN price ELSE NULL END), 
   currency
 FROM "Transaction"
 GROUP BY "asset", currency
